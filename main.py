814872583a20649946476e5cec2ed19c5c483e7e
santiago <fernandovelandotuano69@gmail.com>
2023-05-03 01:41:33 +0200

    Class8-1

---
+++ main.py
@@ -0,0 +1,30 @@
+#import problema1
+
+
+edad = 12
+altura = 1.79
+nombre = "juan"
+estado = True
+
+
+lista1 = (10,5,3,9)
+lista2 = (1.78,2.66,1.55,89.4)
+lista3 = ("Lunes","Martes","Miercoles")
+lista4 = ("juan",45,1.92, False)
+
+
+
+
+
+if __name__ == '__main__':
+    print(len(lista1))
+    print(len(lista2))
+    print(len(lista3))
+    print(len(lista4))
+
+    print()
+    print(lista1)
+    print()
+    #problema1.sumar_5_enteros()
+    print()
+    #alumnos()
